6d18a2050dc51efd7e339c6fb1d22318b8325113
mishalp <mail4mishal@gmail.com>
2023-11-05 21:55:15 +0530

    server changes

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://pdf-wiz-backend.onrender.com',
         secure: false,
       },
     },
