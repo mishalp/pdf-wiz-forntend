28ca9a67a719d5ac803bb7f3db8ef31e474fe8eb
mishalp <mail4mishal@gmail.com>
2023-11-06 11:46:03 +0530

    bug fix

--- src/pages/download/DownloadPdf.jsx
+++ src/pages/download/DownloadPdf.jsx
@@ -1,14 +1,24 @@
 import { saveAs } from 'file-saver'
-import { useLocation } from 'react-router-dom'
+import { useState } from 'react'
+import { useLocation, useSearchParams } from 'react-router-dom'
 
 export default function DownloadPdf() {
     const { state } = useLocation()
+    const [loading, setLoading] = useState(false)
+
+    const download = async() => {
+        setLoading(true)
+        saveAs(state, "New.pdf")
+         setTimeout(()=>{setLoading(false)}, 3000)
+    }
+
     return (
         <div className='p-4 pt-40 flex flex-col gap-4 items-center justify-center'>
             <p>Your PDF is Ready to Download</p>
             <button
-                className='p-6 px-8 bg-secondary rounded-md text-primary font-medium text-3xl'
-                onClick={() => saveAs(state, "New.pdf")}
+                disabled={loading}
+                className='p-6 px-8 bg-secondary disabled:opacity-80 duration-100 rounded-md text-primary font-medium text-3xl'
+                onClick={download}
             >
                 Download
             </button>
